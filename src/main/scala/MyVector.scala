066e47cb0b57223f391e8d255228dfbc58f634e2
Tuomo Nappa <tuomo.nappa@aalto.fi>
2022-03-12 11:23:24 +0200

    Add MyVector for vector calculations

---
+++ src/main/scala/MyVector.scala
@@ -0,0 +1,37 @@
+import scala.math._
+
+class MyVector(val x: Double, val y: Double, val z: Double) {
+  def asUnit: MyVector = {
+    this * (1 / this.lenght)
+  }
+
+  // cross product of this x other
+  def crossProduct(other: MyVector): MyVector = {
+    val crossX = this.y * other.z - this.z * other.y
+    val crossY = this.z * other.x - this.x * other.z
+    val crossZ = this.x * other.y - this.y * other.x
+    new MyVector(crossX, crossY, crossZ)
+  }
+
+  def lenght: Double = {
+    sqrt(pow(x,2) + pow(y,2) + pow(z,2))
+  }
+
+  // scalar product
+  def *(a: Double): MyVector = {
+    new MyVector(a*x, a*y, a*z)
+  }
+
+  // dot product
+  def *(other: MyVector): Double = {
+    this.x * other.x + this.y * other.y + this.z * other.z
+  }
+
+  def -(other: MyVector): MyVector = {
+    new MyVector(this.x - other.x, this.y - other.y, this.z - other.z)
+  }
+
+  def +(other: MyVector): MyVector = {
+    new MyVector(this.x + other.x, this.y + other.y, this.z + other.y)
+  }
+}

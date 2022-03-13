adb554c5626eb348954543ed056be2c8ce14cbe5
Tuomo Nappa <tuomo.nappa@aalto.fi>
2022-03-13 16:30:58 +0200

    Add toString method and require LightRays to have non-zero direction

--- src/main/scala/LightRay.scala
+++ src/main/scala/LightRay.scala
@@ -1,5 +1,8 @@
 import java.awt.Color
 
-class LightRay(x: Double, y: Double, z: Double, val origin: MyVector, val color: Color) extends MyVector(x, y, z) {
+class LightRay(val direction: MyVector, val origin: MyVector, val color: Color = Color.WHITE) extends MyVector(direction.x, direction.y, direction.z) {
+  require(direction.lenght != 0, "LightRay must have a non-zero direction")
+
+  override def toString: String = s"(${direction.toString})x + (${origin.toString}), color: [r=${color.getRed},g=${color.getGreen},b=${color.getBlue}]"
 
 }

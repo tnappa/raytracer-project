6585c308692eb71b502b2735984794706d4d4739
Tuomo Nappa <tuomo.nappa@aalto.fi>
2022-04-10 22:57:01 +0300

    Use MyColor instead of java.awt.Color

--- src/main/Raytracer/LightRay.scala
+++ src/main/Raytracer/LightRay.scala
@@ -1,10 +1,14 @@
 package Raytracer
 
-import java.awt.Color
-
-class LightRay(val direction: MyVector, val origin: MyVector, val color: Color = Color.WHITE) extends MyVector(direction.x, direction.y, direction.z) {
+class LightRay(val direction: MyVector, val origin: MyVector) extends MyVector(direction.x, direction.y, direction.z) {
   require(direction.lenght != 0, "LightRay must have a non-zero direction")
 
-  override def toString: String = s"(${direction.toString})x + (${origin.toString}), color: [r=${color.getRed},g=${color.getGreen},b=${color.getBlue}]"
+  override def toString: String = s"(${direction.toString})x + (${origin.toString}), color: [${color.toString}]"
+
+  private var col: MyColor = MyColor.WHITE
+
+  def color = col
+
+  def setColor(color: MyColor): Unit = col = color
 
 }

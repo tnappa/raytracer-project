6c892450582d6d548a1e83a84a6efb12fef6ce43
Tuomo Nappa <tuomo.nappa@aalto.fi>
2022-04-10 22:55:51 +0300

    Add companion object and toString method

--- src/main/Raytracer/MyColor.scala
+++ src/main/Raytracer/MyColor.scala
@@ -2,8 +2,12 @@ package Raytracer
 
 import java.awt.Color
 
-// r, g and b values are between 0 and 1.0
+// r, g and b values are multipliers for the amount of color
+// 0.0: no color, 1.0: max color value that can be displayed
+// values can exceed 1.0 but when converting to awt.Color values above 1.0 are reduced to 1.0
 class MyColor(val r: Double, val g: Double, val b: Double) {
+
+  override def toString: String = s"red: ${r}, green: ${g}, blue: ${b}"
 
   def this(color: Color) = {
     this(color.getRed / 255.0, color.getGreen / 255.0, color.getBlue / 255.0)
@@ -28,4 +32,10 @@ class MyColor(val r: Double, val g: Double, val b: Double) {
   def asAwtColor: Color = {
     new Color(r.toFloat min 1.0F, g.toFloat min 1.0F, b.toFloat min 1.0F)
   }
+}
+
+object MyColor {
+  def BLACK = new MyColor(0, 0, 0)
+  def WHITE = new MyColor(1, 1, 1)
+  def apply(color: Color) = new MyColor(color)
 }
